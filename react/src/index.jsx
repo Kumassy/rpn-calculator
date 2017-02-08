76588a7a663ac3d73e1d63f7d3b7abc6389155aa
Kumassy <kumassyii@gmail.com>
2017-02-08 22:50:11 +0900

    update styling

--- react/src/index.jsx
+++ react/src/index.jsx
@@ -3,14 +3,28 @@ import {render} from 'react-dom';
 import ReactDOM from 'react-dom';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import AppBar from 'material-ui/AppBar';
+import {
+  grey100, grey300, grey400, grey600,
+  orange700, orange500, orange400
+} from 'material-ui/styles/colors';
 
 const RPN = require('./rpn');
 
 injectTapEventPlugin();
 
+const muiTheme = getMuiTheme({
+  palette: {
+    borderColor: grey600,
+    primary1Color: orange700,
+    primary2Color: orange500,
+    primary3Color: orange400,
+  },
+});
 
 
 class RPNRow extends React.Component {
@@ -20,7 +34,7 @@ class RPNRow extends React.Component {
 
   render () {
     return (
-      <div>
+      <div className="rpn-row">
         <div className="expression">{this.props.expression}</div>
         <div className="result">{RPN.solveRPN(this.props.expression)}</div>
       </div>
@@ -34,7 +48,7 @@ class RPNApp extends React.Component {
   constructor (props){
     super(props);
     this.state = {
-      userInput: 'Hello!',
+      userInput: '',
       inputHistory: ["1 2 *", "3 4 5 - *"]
     };
 
@@ -70,15 +84,22 @@ class RPNApp extends React.Component {
       return <RPNRow key={input} expression={input}></RPNRow>;
     });
     return (
-      <MuiThemeProvider>
-        <div>
-          <TextField
-            floatingLabelText="Input RPN Expression"
-            value={this.state.userInput}
-            onChange={this._handleChanged}
-            onKeyDown={this._handleKeyDown}
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div className="rpn-app">
+          <AppBar
+            title="RPN Calculator"
+            showMenuIconButton={false}
           />
-          <ul>{rows}</ul>
+          <div className="wrapper">
+            <TextField
+              className="text-field"
+              floatingLabelText="Input RPN Expression"
+              value={this.state.userInput}
+              onChange={this._handleChanged}
+              onKeyDown={this._handleKeyDown}
+            />
+            <div className="history">{rows}</div>
+          </div>
         </div>
       </MuiThemeProvider>
     );
